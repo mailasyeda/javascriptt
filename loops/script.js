3b92b718be017b57fb7a6eaf6436d27aa84e2cf1
Syeda Maila <syedamailamansoor@gmail.com>
2026-07-17 17:50:39 +0500

    push

---
+++ loops/script.js
@@ -0,0 +1,22 @@
+for (let i=1; i<=10; i++){
+  console.log(i);
+};
+
+for (let i2=1; i2<=20; i2++){
+    if(i2 % 2 === 0)
+        console.log(i2);
+}
+
+let total = 0;
+for ( let i3=1; i3<=100;  i3++){
+    total= total+i3;
+}
+
+console.log(total);
+
+
+let line ="I am happy";
+console.log(line.length);
+console.log(line.toUpperCase);
+console.log(line.toLocaleLowerCase);
+console.log(line.slice(0, 4));
