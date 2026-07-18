c776fb0b2dcc2e91611b8da7f765426f10a041c5
Syeda Maila <syedamailamansoor@gmail.com>
2026-07-18 15:15:58 +0500

    changes

--- practice loops/script.js
+++ practice loops/script.js
@@ -19,3 +19,56 @@ for (let i=1; i <=rows; i++) {
 document.getElementById("starResult").innerText= output;
 resultBox.style.display = "block";
 }
+
+
+
+
+
+
+
+function drawnTable() {
+    let table = Number(document.getElementById("tablenumber").value);
+    let tablebox =document.getElementById("tableResult");
+     
+    if(!table){
+        document.getElementById("tableResult").textContent= "Enter a valid number!";
+        tablebox.style.display="block";
+        return;
+    }
+    let output=""
+    for(let i = 1; i<=10; i++){
+        output += `${table} x ${i} = ${table*i}\n`
+    }
+    document.getElementById("tableResult").innerText= output;
+    tablebox.style.display="block";
+}
+
+
+
+
+
+
+
+
+function showGrid() {
+    let size = Number(document.getElementById("gridsize"). value);
+    let gridbox = document.getElementById("gridResult");
+    if(!size || size <1 ){
+        document.getElementById("gridResult").innerText="Enter a number greater than 0!";
+        gridbox.style.display="block";
+        return;
+
+    }
+    let output = ""
+    for(let row =1; row <= size; row++){
+        let line = ""
+    
+        for(let col = 1; col <= row; col++) {
+            line += (row * col).toString().padStart(4, " ");
+        }
+        output += line + "\n";
+    }
+
+        document.getElementById("gridResult").innerText= output;
+        gridbox.style.display="block";
+}
